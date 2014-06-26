c4b075e13325fb84f2bae870a2fda2a4ee30bc4c
arcotick <arcotick@gmail.com>
2014-06-26 11:46:00 +0200

    Changes

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ var altura, random;
 window.onload = function(){
 	setInterval(
 		function(){
+			document.getElementById('time').innerHTML = abstime/10;
 			if (abstime % 2000 == 0) {
 				random = Math.random();
 			}
@@ -33,6 +34,8 @@ window.onload = function(){
 			}
 			if (parseInt(ball.style.top) > h-r) {
 				ball.style.top = h-r + 'px';
+				finished = true;
+				ball.style.top = h-r + 'px';
 			}
 		},
 		10)
@@ -41,32 +44,16 @@ var mouseDown = function() {
 	started = true;
 	altura = h-r-parseInt(ball.style.top);
 	time = 0;
+	if (finished) {
+		abstime = 0;
+		time = 0;
+		finished = false;
+		started = false;
+	};
 }
 function generateWall(t, random) {
 	wallT.style.top = random*h-h - 2*r + 'px';
 	wallB.style.top = random*h + 2*r + 'px';
 	wallB.style.left = wallT.style.left = w-t*(w+r) + 'px';
 }
-var overlaps = (function () {
-    function getPositions( elem ) {
-        var pos, width, height;
-        pos = $( elem ).position();
-        width = $( elem ).width() / 2;
-        height = $( elem ).height();
-        return [ [ pos.left, pos.left + width ], [ pos.top, pos.top + height ] ];
-    }
-
-    function comparePositions( p1, p2 ) {
-        var r1, r2;
-        r1 = p1[0] < p2[0] ? p1 : p2;
-        r2 = p1[0] < p2[0] ? p2 : p1;
-        return r1[1] > r2[0] || r1[0] === r2[0];
-    }
-
-    return function ( a, b ) {
-        var pos1 = getPositions( a ),
-            pos2 = getPositions( b );
-        return comparePositions( pos1[0], pos2[0] ) && comparePositions( pos1[1], pos2[1] );
-    };
-})();
 window.addEventListener('mousedown', mouseDown)
