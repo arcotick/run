acfaf803701a6c55b15125877c3052a57f85da5f
arcotick <arcotick@gmail.com>
2014-06-26 03:35:18 +0200

    Minor changes

--- main.js
+++ main.js
@@ -12,20 +12,20 @@ ball.style.left = (w-r)/2 + 'px';
 ball.style.top = h - r + 'px';
 ball.style.width = ball.style.height = r + 'px';
 
-var started = false;
+var started = false,
+	finished = false;
 
 var time = 0, abstime = 0, t;
 var altura, random;
 
 window.onload = function(){
-	ball.style.left = 0 + 'px';
 	setInterval(
 		function(){
 			if (abstime % 2000 == 0) {
 				random = Math.random();
 			}
-			generateBlock(((abstime%2000)-1)/2000, random*(h-4*r)/h + 2*r/h);
-			if(started){
+			generateWall(((abstime%2000)-1)/2000, random*(h-4*r)/h + 2*r/h);
+			if(started && !finished){
 				abstime += 10;
 				time+=10;
 				t=time/100;
@@ -41,11 +41,32 @@ var mouseDown = function() {
 	started = true;
 	altura = h-r-parseInt(ball.style.top);
 	time = 0;
-	bounce = 0;
 }
-function generateBlock(t, random) {
+function generateWall(t, random) {
 	wallT.style.top = random*h-h - 2*r + 'px';
 	wallB.style.top = random*h + 2*r + 'px';
 	wallB.style.left = wallT.style.left = w-t*(w+r) + 'px';
 }
+var overlaps = (function () {
+    function getPositions( elem ) {
+        var pos, width, height;
+        pos = $( elem ).position();
+        width = $( elem ).width() / 2;
+        height = $( elem ).height();
+        return [ [ pos.left, pos.left + width ], [ pos.top, pos.top + height ] ];
+    }
+
+    function comparePositions( p1, p2 ) {
+        var r1, r2;
+        r1 = p1[0] < p2[0] ? p1 : p2;
+        r2 = p1[0] < p2[0] ? p2 : p1;
+        return r1[1] > r2[0] || r1[0] === r2[0];
+    }
+
+    return function ( a, b ) {
+        var pos1 = getPositions( a ),
+            pos2 = getPositions( b );
+        return comparePositions( pos1[0], pos2[0] ) && comparePositions( pos1[1], pos2[1] );
+    };
+})();
 window.addEventListener('mousedown', mouseDown)
